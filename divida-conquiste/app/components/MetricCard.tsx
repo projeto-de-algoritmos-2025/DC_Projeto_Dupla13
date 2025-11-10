0f248a2f9d5d4b992910b543b295e996e5a6b832
UnderwaterVillager <vjbfvinicius@hotmail.com>
2025-11-10 20:36:01 -0300

    Renderização  de cartões numéricos após cliente carregado

--- divida-conquiste/app/components/MetricCard.tsx
+++ divida-conquiste/app/components/MetricCard.tsx
@@ -1,9 +1,19 @@
+import { useState, useEffect } from "react";
+
 export default function MetricCard({ title, value}: { title: string; value: string}) {
+  
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => setIsClient(true), []);
+
   return (
     <div className="rounded-xl p-6 bg-white shadow flex flex-col">
-      <span className="text-sm text-gray-500">{title}</span>
-      <strong className="text-3xl">{value}</strong>
-    </div>
+      { isClient &&
+        <>
+          <span className="text-sm text-gray-500">{title}</span>
+          <strong className="text-3xl">{value}</strong>
+        </>
+      }
+      </div>
   );
 }
 
